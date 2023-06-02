7a3367a108c361076c3806131d836995ee5734a9
Shayan Nosratzadegan <shayan@MBP-von-Shayan.fritz.box>
2023-06-02 12:40:40 +0200

    adds typeorm, adds typeorm entities, adds relations, adds pagination

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestj
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto/update-coffee.dto';
+import { PaginationQueryDto } from 'src/common/dto/pagination-query-dto/pagination-query-dto';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -10,8 +11,8 @@ export class CoffeesController {
 
     }
     @Get()
-    findAll(@Query() paginationQuery){
-        return this.coffeeService.findAll();
+    findAll(@Query() paginationQuery: PaginationQueryDto){
+        return this.coffeeService.findAll(paginationQuery);
     }
 
     @Get(':id')
--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Coffee } from './entities/coffee.entity';
+import { Flavor } from './entities/flavor.entity/flavor.entity';
 
-@Module({controllers: [CoffeesController], providers: [CoffeesService]})
+@Module({imports: [TypeOrmModule.forFeature([Coffee, Flavor])], controllers: [CoffeesController], providers: [CoffeesService]})
 export class CoffeesModule {}
