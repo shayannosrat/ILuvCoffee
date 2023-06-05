2246471b8b474e3bfd337042d41f932c23d90ce3
Shayan <shayan.nosrat@t-online.de>
2023-06-05 11:07:50 +0200

    adds mongodb connection

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,6 +1,17 @@
 import { Module } from '@nestjs/common';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
+import { Coffee, CoffeeSchema } from './entities/coffee.entity';
+import { MongooseModule } from '@nestjs/mongoose';
 
-@Module({controllers: [CoffeesController], providers: [CoffeesService]})
-export class CoffeesModule {}
+@Module({
+    imports: [
+        MongooseModule.forFeature([
+            {
+                name: Coffee.name,
+                schema: CoffeeSchema,
+            }
+        ])
+    ], controllers: [CoffeesController], providers: [CoffeesService]
+})
+export class CoffeesModule { }
--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,44 +1,77 @@
-import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectConnection, InjectModel } from '@nestjs/mongoose';
+import { Connection, Model } from 'mongoose';
+import { Coffee } from './entities/coffee.entity';
+import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto/pagination-query.dto';
+import { CreateCoffeeDto } from './dto/create-coffee.dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto/update-coffee.dto';
 
 @Injectable()
 export class CoffeesService {
-    private coffees = [
-        {
-            id: 1,
-            name: 'Shipwreck Roast',
-            brand: 'Buddy Brew',
-            flavors: ['chocolate', 'vanilla'],
-        }
-    ];
+  constructor(
+    @InjectModel(Coffee.name) private readonly coffeeModel: Model<Coffee>,
+    @InjectConnection() private readonly connection: Connection,
+    @InjectModel(Event.name) private readonly eventModel: Model<Event>,
+  ) {}
 
-    findAll(){
-        return this.coffees;
-    }
+  findAll(paginationQuery: PaginationQueryDto) {
+    const { limit, offset } = paginationQuery;
+    return this.coffeeModel
+      .find()
+      .skip(offset)
+      .limit(limit)
+      .exec();
+  }
 
-    findOne(id: string){
-        const coffee =  this.coffees.find(item => item.id === +id);
-        if(!coffee){
-            throw new NotFoundException(`Coffee #${id} not found`);
-        }
-        return coffee;
+  async findOne(id: string) {
+    const coffee = await this.coffeeModel.findOne({ _id: id }).exec();
+    if (!coffee) {
+      throw new NotFoundException(`Coffee #${id} not found`);
     }
+    return coffee;
+  }
 
-    create(createCoffeeDto: any){
-        this.coffees.push(createCoffeeDto);
-        return createCoffeeDto;
-    }
+  create(createCoffeeDto: CreateCoffeeDto) {
+    const coffee = new this.coffeeModel(createCoffeeDto);
+    return coffee.save();
+  }
 
-    update(id: string, updateCoffeeDto: any){
-        const existingCoffee = this.findOne(id);
-        if(existingCoffee){
-            // update
-        }
-    }
+  async update(id: string, updateCoffeeDto: UpdateCoffeeDto) {
+    const existingCoffee = await this.coffeeModel
+      .findOneAndUpdate({ _id: id }, { $set: updateCoffeeDto }, { new: true })
+      .exec();
 
-    remove(id: string){
-        const coffeeIndex = this.coffees.findIndex(item => item.id === +id);
-        if(coffeeIndex >= 0){
-            this.coffees.splice(coffeeIndex, 1);
-        }
+    if (!existingCoffee) {
+      throw new NotFoundException(`Coffee #${id} not found`);
     }
+    return existingCoffee;
+  }
+
+  async remove(id: string) {
+    const coffee = await this.findOne(id);
+    //return coffee.remove();
+  }
+
+  async recommendCoffee(coffee: Coffee) {
+    const session = await this.connection.startSession();
+    session.startTransaction();
+
+    try {
+      coffee.recommendations++;
+
+      const recommendEvent = new this.eventModel({
+        name: 'recommend_coffee',
+        type: 'coffee',
+        payload: { coffeeId: coffee.id },
+      });
+      await recommendEvent.save({ session });
+      await coffee.save({ session });
+
+      await session.commitTransaction();
+    } catch (err) {
+      await session.abortTransaction();
+    } finally {
+      session.endSession();
+    }
+  }
 }
